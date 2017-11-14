fd469fa3f926e46298812845d97ade6484ecd7f8
Karolina Dubitskaya <Karolina.D100899@gmail.com>
2017-11-14 15:05:46 +0300

    Added menu items: open, save as, close and exit.

--- Image Editor/Image Editor/MainWindow.xaml.cs
+++ Image Editor/Image Editor/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Drawing;
+using System.Windows.Interop;
+using System.IO;
 
 namespace Image_Editor
 {
@@ -20,9 +23,88 @@ namespace Image_Editor
     /// </summary>
     public partial class MainWindow : Window
     {
+         
+
+        FileProcessing fileProcessing;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            fileProcessing = new FileProcessing();
+
+            fileProcessing.FileOpened += ReceiveImage;
+        }
+
+        private Bitmap originalImage;
+        private Bitmap currentImage;
+        private Bitmap backupImage;
+
+        private void ReceiveImage(object sender, FileProcessing.OpenEventArgs e)
+        {
+            if (e.Input == null)
+                return;
+
+            originalImage = e.Input;
+            currentImage = new Bitmap(originalImage);
+            backupImage = new Bitmap(originalImage);
+        }
+
+        private void BackUpCurrentImage()
+        {
+            backupImage = (Bitmap)currentImage.Clone();
+        }
+
+        private void RestoreBackupImage()
+        {
+            currentImage = (Bitmap)backupImage.Clone();
+        }
+
+        BitmapImage BitmapToImageSource(Bitmap bitmap)
+        {
+            using (MemoryStream memory = new MemoryStream())
+            {
+                bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+                memory.Position = 0;
+                BitmapImage bitmapimage = new BitmapImage();
+                bitmapimage.BeginInit();
+                bitmapimage.StreamSource = memory;
+                bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapimage.EndInit();
+
+                return bitmapimage;
+            }
+        }
+
+        private void ViewOriginalImage()
+        {
+            image.Source = BitmapToImageSource(originalImage);
+        }
+
+        private void ViewCurrentImage()
+        {
+            image.Source = BitmapToImageSource(currentImage);
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            image.Source = null;
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void SaveAs_Click(object sender, RoutedEventArgs e)
+        {
+            fileProcessing.SaveFileAs(currentImage);
+        }
+
+        private void Open_Click(object sender, RoutedEventArgs e)
+        {
+            fileProcessing.OpenFile();
+            ViewOriginalImage();
         }
     }
 }
